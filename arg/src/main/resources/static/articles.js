de299e30dea0b2be98f3814bf02c14761bd9f673
DanJBee <danielbee912@gmail.com>
2026-05-09 22:51:22 +0100

    adjusted intervals of articles and information sources to be shorter

--- arg/src/main/resources/static/articles.js
+++ arg/src/main/resources/static/articles.js
@@ -3,47 +3,47 @@ function sleep(ms) {
 }
 
 async function articles() {
-    await sleep(3000);
+    await sleep(2000);
 
     window.open("https://78.141.238.111:8080/stop");
 
-    await sleep(2000);
+    await sleep(1000);
     
     window.open("https://78.141.238.111:8080/doing");
 
-    await sleep(2000);
+    await sleep(1000);
 
     window.open("https://78.141.238.111:8080/what");
 
-    await sleep(2000);
+    await sleep(1000);
 
     window.open("https://78.141.238.111:8080/youre");
 
-    await sleep(2000);
+    await sleep(1000);
 
     window.open("https://78.141.238.111:8080/told");
 
-    await sleep(3000);
+    await sleep(2000);
 
     window.open("https://78.141.238.111:8080/do");
 
-    await sleep(2000);
+    await sleep(1000);
 
     window.open("https://78.141.238.111:8080/you");
 
-    await sleep(2000);
+    await sleep(1000);
 
     window.open("https://78.141.238.111:8080/know");
 
-    await sleep(2000);
+    await sleep(1000);
 
     window.open("https://78.141.238.111:8080/what-damage");
 
-    await sleep(2000);
+    await sleep(1000);
 
     window.open("https://78.141.238.111:8080/youve");
 
-    await sleep(2000);
+    await sleep(1000);
 
     window.open("https://78.141.238.111:8080/done");
 }
