d77c341016314b6d43b8bb7bb02803f80b0224ce
DanJBee <danielbee912@gmail.com>
2026-05-09 21:52:59 +0100

    fix article redirects

--- arg/src/main/resources/static/articles.js
+++ arg/src/main/resources/static/articles.js
@@ -5,47 +5,47 @@ function sleep(ms) {
 async function articles() {
     await sleep(3000);
 
-    window.open("https://localhost:8080/stop");
+    window.open("https://78.141.238.111:8080/stop");
 
     await sleep(2000);
     
-    window.open("https://localhost:8080/doing");
+    window.open("https://78.141.238.111:8080/doing");
 
     await sleep(2000);
 
-    window.open("https://localhost:8080/what");
+    window.open("https://78.141.238.111:8080/what");
 
     await sleep(2000);
 
-    window.open("https://localhost:8080/youre");
+    window.open("https://78.141.238.111:8080/youre");
 
     await sleep(2000);
 
-    window.open("https://localhost:8080/told");
+    window.open("https://78.141.238.111:8080/told");
 
     await sleep(3000);
 
-    window.open("https://localhost:8080/do");
+    window.open("https://78.141.238.111:8080/do");
 
     await sleep(2000);
 
-    window.open("https://localhost:8080/you");
+    window.open("https://78.141.238.111:8080/you");
 
     await sleep(2000);
 
-    window.open("https://localhost:8080/know");
+    window.open("https://78.141.238.111:8080/know");
 
     await sleep(2000);
 
-    window.open("https://localhost:8080/what-damage");
+    window.open("https://78.141.238.111:8080/what-damage");
 
     await sleep(2000);
 
-    window.open("https://localhost:8080/youve");
+    window.open("https://78.141.238.111:8080/youve");
 
     await sleep(2000);
 
-    window.open("https://localhost:8080/done");
+    window.open("https://78.141.238.111:8080/done");
 }
 
 document.addEventListener('DOMContentLoaded', () => {
