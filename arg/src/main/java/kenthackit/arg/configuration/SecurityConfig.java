b4bb79cf43bd9bf0f5ff804406186dc818413fa2
DanJBee <danielbee912@gmail.com>
2026-05-09 20:53:25 +0100

    applied more fixes for CSS not working on login page

--- arg/src/main/java/kenthackit/arg/configuration/SecurityConfig.java
+++ arg/src/main/java/kenthackit/arg/configuration/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
             .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-            .requestMatchers("/login", "/stop", "/doing", "/what", "/youre", "/told", "/secret", "/css/**", "/js/**", "/images/**", "/static/**").permitAll()
+            .requestMatchers("/login", "/style.css", "/greeting-style.css", "/stop", "/doing", "/what", "/youre", "/told", "/secret", "/css/**", "/js/**", "/images/**", "/static/**").permitAll()
             .anyRequest().authenticated()
         )
         .formLogin(form -> form
@@ -41,6 +41,6 @@ public class SecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring()
-            .requestMatchers("/css/**", "/js/**", "/images/**", "/static/**", "/favicon.ico");
+            .requestMatchers("/style.css", "/favicon.ico", "/css/**", "/js/**", "/images/**");
     }
 }
