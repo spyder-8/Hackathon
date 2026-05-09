aee8f3966a48dc96a9d9425680d84bcbda338043
DanJBee <danielbee912@gmail.com>
2026-05-09 11:17:33 +0100

    initial backend setup

---
+++ arg/src/main/java/kenthackit/arg/ArgApplication.java
@@ -0,0 +1,13 @@
+package kenthackit.arg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgApplication.class, args);
+	}
+
+}
